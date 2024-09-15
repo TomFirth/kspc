d3685b108f88090ccec17c4b19dc98d613bce9aa
TomFirth <tom.firth84@gmail.com>
2024-09-15 10:02:48 +0100

    shuffle

--- client/App.js
+++ client/App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import ChatScreen from './screens/ChatScreen';
-import ShareScreen from './screens/ShareScreen';
+import Home from './screens/Home';
+import Chat from './screens/Chat';
+import Share from './screens/Share';
 
 const Stack = createStackNavigator();
 
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-        <Stack.Screen name="Share" component={ShareScreen} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Chat" component={Chat} />
+        <Stack.Screen name="Share" component={Share} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- client/README.md
+++ client/README.md
@@ -3,14 +3,14 @@
 ## Run the App
 
 ### iOS (on macOS): 
-Run: `npx react-native run-ios`
+Run: `cd /client/ && npx react-native run-ios`
 Alternatively, you can open the project in Xcode and run it from there by pressing the "Play" button.
 
 ### Android:
-Make sure your Android emulator is running or your Android device is connected, then run: `npx react-native run-android`
+Make sure your Android emulator is running or your Android device is connected, then run: `cd /client/ && npx react-native run-android`
 
 ### Development Server
-React Native uses Metro to bundle JavaScript. Run: `npx react-native start`
+React Native uses Metro to bundle JavaScript. Run: `cd /client/ && npx react-native start`
 
 Optional Tools:
 Expo CLI: You can also use Expo, a toolchain built around React Native, which makes development easier. Expo handles many configurations for you. Install Expo globally: `npm install -g expo-cli`
