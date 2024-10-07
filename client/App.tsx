6004ba624c2d1e6a7576e004e475a912b23e851c
TomFirth <tom.firth84@gmail.com>
2024-10-07 23:22:56 +0100

    no longer expo

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { requestPermissions } from './Android/permissions';
 
 export default function App() {
-  useEffect(() => {
-    requestPermissions();
-  }, []);
-
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
